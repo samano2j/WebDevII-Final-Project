97ca0805456a042ab462eddf70e769895bc7d760
Kazuki Matsuo <kazumatsu.dev28@gmail.com>
2023-03-03 08:28:55 +0900

    improve performance

--- src/random.js
+++ src/random.js
@@ -7,14 +7,15 @@ Input:  limit - number of gifs to return
 Return: GIFs of random results
 */
 export const fetchRandomGIFs = async (limit = 1) => {
-  const gifList = []
   const url = 'https://api.giphy.com/v1/gifs/random'
   const params = { api_key: API_KEY }
+  const promiseList = []
 
   for(let i = 0; i < limit; i++) {
-      const { data: gifData } = await axios.get(url, { params })
-      gifList.push(gifData.data)
+    promiseList.push(axios.get(url, { params }))
   }
 
-  return gifList
+  const gifList = await Promise.all(promiseList)
+
+  return gifList.map(gif => gif.data.data)
 }
