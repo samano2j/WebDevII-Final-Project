c63afaa9943468bd2296960965963f2444f12001
samano2j <jemsamano@yahoo.com>
2023-03-02 19:16:02 -0600

    Added Additional API features

---
+++ src/categories.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { API_KEY } from "./apiKey";
+
+/*
+Categories function
+Return: list of Gif categories
+*/
+export const fetchCategoryGIFs = async () => {
+    const url = 'https://api.giphy.com/v1/gifs/categories'
+    const params = { api_key: API_KEY }
+
+    const { data: gifData } = await axios.get(url, { params })
+
+    console.log(gifData)
+    return gifData.data
+}
