8c94d38cd81a67f0b38f07d440d195486a45252f
Kazuki Matsuo <kazumatsu.dev28@gmail.com>
2023-03-04 23:58:15 +0900

    update: make npm start work

--- src/js/hamburgermenu.js
+++ src/js/hamburgermenu.js
@@ -1,7 +1,7 @@
-function toggleNav() {
-  var body = document.body;
-  var hamburger = document.getElementById("js-hamburger");
-  var blackBg = document.getElementById("js-black-bg");
+export function toggleNav() {
+  const body = document.body;
+  const hamburger = document.getElementById("js-hamburger");
+  const blackBg = document.getElementById("js-black-bg");
 
   hamburger.addEventListener("click", function () {
     body.classList.toggle("nav-open");
@@ -10,4 +10,3 @@ function toggleNav() {
     body.classList.remove("nav-open");
   });
 }
-toggleNav();
